6baaed3671ba2f373f35b7a71025476795921562
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-04-09 08:47:12 +0200

    added seed support for random shuffle

--- src/test/groovy/com/github/wololock/RandomizedOrderExtension.groovy
+++ src/test/groovy/com/github/wololock/RandomizedOrderExtension.groovy
@@ -5,11 +5,19 @@ import org.spockframework.runtime.model.SpecInfo
 
 final class RandomizedOrderExtension extends AbstractAnnotationDrivenExtension<RandomizedOrder> {
 
+    public static final String SPOCK_RANDOM_ORDER_SEED = "spock.random.order.seed"
+
     @Override
     void visitSpecAnnotation(RandomizedOrder annotation, SpecInfo spec) {
+        final long seed = System.getProperty(SPOCK_RANDOM_ORDER_SEED)?.toLong() ?: System.nanoTime()
+
+        final Random random = new Random(seed)
+
+        println "Random seed used: ${seed}\nYou can re-run the test with predefined seed by passing -D${SPOCK_RANDOM_ORDER_SEED}=${seed}\n\n"
+
         final List<Integer> order = (0..(spec.features.size())) as ArrayList
 
-        Collections.shuffle(order)
+        Collections.shuffle(order, random)
 
         spec.features.each { feature ->
             feature.executionOrder = order.pop()
