86f3d5050e302a3db025c72a660a9926da3c789c
Szymon Stepniak <szymon.stepniak@gmail.com>
2019-04-09 09:17:03 +0200

    updated random seed usage to a static constant (otherwise each test uses a different seed)

--- src/test/groovy/com/github/wololock/RandomizedOrderExtension.groovy
+++ src/test/groovy/com/github/wololock/RandomizedOrderExtension.groovy
@@ -7,14 +7,16 @@ final class RandomizedOrderExtension extends AbstractAnnotationDrivenExtension<R
 
     public static final String SPOCK_RANDOM_ORDER_SEED = "spock.random.order.seed"
 
+    private static final long seed = System.getProperty(SPOCK_RANDOM_ORDER_SEED)?.toLong() ?: System.nanoTime()
+
+    private static final Random random = new Random(seed)
+
+    static {
+        println "Random seed used: ${seed}\nYou can re-run the test with predefined seed by passing -D${SPOCK_RANDOM_ORDER_SEED}=${seed}\n\n"
+    }
+
     @Override
     void visitSpecAnnotation(RandomizedOrder annotation, SpecInfo spec) {
-        final long seed = System.getProperty(SPOCK_RANDOM_ORDER_SEED)?.toLong() ?: System.nanoTime()
-
-        final Random random = new Random(seed)
-
-        println "Random seed used: ${seed}\nYou can re-run the test with predefined seed by passing -D${SPOCK_RANDOM_ORDER_SEED}=${seed}\n\n"
-
         final List<Integer> order = (0..(spec.features.size())) as ArrayList
 
         Collections.shuffle(order, random)
